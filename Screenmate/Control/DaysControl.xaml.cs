9137f047562c735b06ca8fe0c8a39d49eaadf516
Asumi <ronan.chaubell@gmail.com>
2023-12-18 17:07:12 +0100

    18/12/2023

--- Screenmate/Control/DaysControl.xaml.cs
+++ Screenmate/Control/DaysControl.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Screenmate.Module;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,16 +18,51 @@ using System.Windows.Shapes;
 
 namespace Screenmate.Control
 {
-    public partial class DaysControl : UserControl
+    public partial class DaysControl : UserControl, INotifyPropertyChanged
     {
         public DaysControl()
         {
             InitializeComponent();
+            IsSelected = false;
+            this.DataContext = this;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private DaysControl selectedItem;
+        public DaysControl SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                if (selectedItem != value)
+                {
+                    selectedItem = value;
+                    OnPropertyChanged(nameof(SelectedItem));
+                }
+            }
+        }
+
+        private bool isSelected;
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set
+            {
+                if (isSelected != value)
+                {
+                    isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                }
+            }
         }
 
         public void days(int numday)
         {
             NumberDays.Content = numday+"";
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ Screenmate/Module/Calendar/Calendrier.xaml.cs
@@ -0,0 +1,162 @@
+﻿using Screenmate.Control;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Screenmate.Module
+{
+    public partial class Calendrier : Window
+    {
+        int month, year;
+
+        public Calendrier()
+        {
+            InitializeComponent();
+        }
+
+        private void Calendrier_Load(object sender, EventArgs e)
+        {
+            DisplayDays();
+        }
+
+        #region Layout Calendar
+        private void DisplayDays()
+        {
+            DateTime now = DateTime.Now;
+            month = now.Month;
+            year = now.Year;
+            string monthName = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            DateMY.Content = char.ToUpper(monthName[0]) + monthName.Substring(1) + " " + year;
+
+            DateTime startMonth = new DateTime(year, month, 1);
+            int days =DateTime.DaysInMonth(year, month);
+            int daysWeek =Convert.ToInt32(startMonth.DayOfWeek.ToString("d"))-1;
+            for (int i =1; i <= daysWeek; i++) 
+            {
+                BlankDaysControl blanckDays = new BlankDaysControl();
+                blanckDays.Margin=new Thickness(1,1,1,0);
+                dayContainer.Children.Add(blanckDays);
+            }
+
+            for (int i = 1; i <= days; i++)
+            {
+                DaysControl daysControl = new DaysControl();
+                daysControl.Margin = new Thickness(1, 1, 1, 0);
+                daysControl.MouseRightButtonDown += OptionsContext_Calendar;
+                daysControl.days(i);
+                dayContainer.Children.Add(daysControl);
+            }
+        }
+
+        private void Previous_Click(object sender, RoutedEventArgs e)
+        {
+            month--;
+            if (month < 1)
+            {
+                month = 12;
+                year--;
+            }
+            dayContainer.Children.Clear();
+            string monthName = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            DateMY.Content = char.ToUpper(monthName[0]) + monthName.Substring(1) + " " + year;
+
+            DateTime startMonth = new DateTime(year, month, 1);
+            int days = DateTime.DaysInMonth(year, month);
+            int daysWeek = Convert.ToInt32(startMonth.DayOfWeek.ToString("d")) - 1;
+
+            for (int i = 1; i <= daysWeek; i++)
+            {
+                BlankDaysControl blanckDays = new BlankDaysControl();
+                blanckDays.Margin = new Thickness(1, 1, 1, 0);
+                dayContainer.Children.Add(blanckDays);
+            }
+
+            for (int i = 1; i <= days; i++)
+            {
+                DaysControl daysControl = new DaysControl();
+                daysControl.Margin = new Thickness(1, 1, 1, 0);
+                daysControl.MouseRightButtonDown += OptionsContext_Calendar;
+                daysControl.days(i);
+                dayContainer.Children.Add(daysControl);
+            }
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            dayContainer.Children.Clear();
+            month++;
+            if (month > 12)
+            {
+                month = 1;
+                year++;
+            }
+            string monthName = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            DateMY.Content = char.ToUpper(monthName[0]) + monthName.Substring(1) + " " + year;
+
+            DateTime startMonth = new DateTime(year, month, 1);
+            int days = DateTime.DaysInMonth(year, month);
+            int daysWeek = Convert.ToInt32(startMonth.DayOfWeek.ToString("d")) - 1;
+
+            for (int i = 1; i <= daysWeek; i++)
+            {
+                BlankDaysControl blanckDays = new BlankDaysControl();
+                blanckDays.Margin = new Thickness(1, 1, 1, 0);
+                dayContainer.Children.Add(blanckDays);
+            }
+
+            for (int i = 1; i <= days; i++)
+            {
+                DaysControl daysControl = new DaysControl();
+                daysControl.Margin = new Thickness(1, 1, 1, 0);
+                daysControl.MouseRightButtonDown += OptionsContext_Calendar;
+                daysControl.days(i);
+                dayContainer.Children.Add(daysControl);
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+        private void OptionsContext_Calendar(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is DaysControl calendarItem)
+            {
+                var itemUnderMouse = e.OriginalSource as FrameworkElement;
+                if (!calendarItem.IsSelected)
+                {
+                    calendarItem.IsSelected = true;
+                }
+
+                calendarItem.SelectedItem = calendarItem;
+                ContextMenu contextMenu = Resources["CalendarContextMenu"] as ContextMenu;
+                contextMenu.PlacementTarget = calendarItem;
+                contextMenu.IsOpen = true;
+                e.Handled = true;
+            }
+        }
+
+        private void AddRappel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DelRappel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
